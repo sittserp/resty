d5832d3810188e12051a26926bdcb6f553cd9891
Perry Sittser <sittserp@gmail.com>
2021-02-05 15:36:04 -0800

    Results Section

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Imput from '../presentation/Input';
+import Results from '../presentation/Results';
 
 export default class App extends React.Component {
   render() {
     return (
       <>
         <Imput />
+        <Results />
       </>
     );
   }
