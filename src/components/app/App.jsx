d9aef2e9fa733acfcdb5ffe0076e4c567a36d461
Perry Sittser <sittserp@gmail.com>
2021-02-05 15:39:51 -0800

    Header Restlify

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Imput from '../presentation/Input';
 import Results from '../presentation/Results';
+import Header from '../presentation/Header';
 
 export default class App extends React.Component {
   render() {
     return (
       <>
+        <Header />
         <Imput />
         <Results />
       </>
