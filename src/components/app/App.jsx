5fc9383862d0849c84a3f882352e53e118fb8d40
Perry Sittser <sittserp@gmail.com>
2021-02-05 15:22:28 -0800

    Input Section

---
+++ src/components/app/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Imput from '../presentation/Input';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Imput />
+      </>
+    );
+  }
+}
---
+++ src/components/presentation/Input.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Input = ({ url, method, body, onSubmit, onChange }) => {
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input 
+          type="text" 
+          name="url" 
+          placeholder="URL" 
+          value={url} 
+          onChange={onChange} />
+
+        <div>
+          <input 
+            type="radio" 
+            id="get" name="method" 
+            value="GET" 
+            checked={method === 'GET'} 
+            onChange={onChange} />
+          <label htmlFor="get">GET</label>
+
+          <input 
+            type="radio" 
+            id="post" 
+            name="method" 
+            value="POST" 
+            checked={method === 'POST'} 
+            onChange={onChange} />
+          <label htmlFor="post">POST</label>
+            
+          <input 
+            type="radio" 
+            id="put" 
+            name="method" 
+            value="PUT" 
+            checked={method === 'PUT'} 
+            onChange={onChange} />
+          <label htmlFor="put">PUT</label> 
+            
+          <input 
+            type="radio" 
+            id="patch" 
+            name="method" 
+            value="PATCH" 
+            checked={method === 'PATCH'} 
+            onChange={onChange} /> 
+          <label htmlFor="patch">PATCH</label>
+            
+          <input 
+            type="radio" 
+            id="delete" 
+            name="method" 
+            value="DELETE" 
+            checked={method === 'DELETE'} 
+            onChange={onChange} />
+          <label htmlFor="delete">DELETE</label>
+        </div>
+        <button>Call</button>
+        <textarea 
+          placeholder="JSON Body" 
+          name="body" 
+          value={body} 
+          onChange={onChange}></textarea>
+      </form>
+    </>
+  );
+};
+
+Input.propTypes = {
+  url: PropTypes.string.isRequired,
+  method: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired
+};
+
+export default Input;
